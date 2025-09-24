ecd7253c3b4d0e0f10776e2e6e5b275823a4837c
Cardoso2626 <sueleidesaraiva@hotmail.com>
2025-09-24 20:50:01 -0300

    Adicionando nova config n security

--- src/main/java/br/com/fiap/sptrint1/configuration/SecurityConfig.java
+++ src/main/java/br/com/fiap/sptrint1/configuration/SecurityConfig.java
@@ -1,29 +1,31 @@
 package br.com.fiap.sptrint1.configuration;
 
-
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return httpSecurity.authorizeHttpRequests(
-                        authorizeRequests ->
-                                authorizeRequests
-                                        .requestMatchers("/").permitAll()
-                                        .requestMatchers("/pageFuncionario/lista").permitAll()
-                                        .anyRequest().authenticated()
-                ).oauth2Login(oauth2 ->
-                        oauth2.defaultSuccessUrl("/pageFuncionario/lista"))
-                .formLogin(Customizer.withDefaults())
-                .build();
-    }
 
+    @Bean
+    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/moto/**", "/chaveiro/**", "/localizacao/**", "/funcionario/**", "/patio/**") // só essas rotas
+                .csrf(csrf -> csrf.disable()) // desabilita CSRF para APIs
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // libera todas
+                .formLogin(form -> form.disable()) // desabilita form login para essas rotas
+                .oauth2Login(oauth -> oauth.disable()) // desabilita OAuth login
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((req, res, e) ->
+                                res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized"))
+                );
+
+        return http.build();
+    }
 }
